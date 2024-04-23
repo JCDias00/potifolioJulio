4283d6a61325405c6eec20db50d4ee6b0d9ae461
Julio Cesar <jccardosodias0@gmail.com>
2024-04-23 19:16:47 -0300

    menu do topo

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
 import { Menu } from '../models/menu';
 import { MenuService } from '../services/menu.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
 
-  constructor(private menuServie: MenuService){}
-  
+  constructor(private menuService: MenuService) { }
+
+
+  menuStatus: string = 'closed';
+
+
+
+  menuItens?: Array<Menu>;
+
+  ngOnInit() {
+    const menu = this.menuService.getItens();
+
+    this.menuItens = menu;
+  }
+
+  menuClick() {
+
+    this.menuStatus = (this.menuStatus === 'open' ? 'closed' : 'open');
+
+  }
+
 }
