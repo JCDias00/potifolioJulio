e79cef623c72b53de9259051aa279a309fd031b9
Julio Cesar <jccardosodias0@gmail.com>
2024-04-29 19:50:59 -0300

    atualizando a descrição

--- src/app/models/profile.ts
+++ src/app/models/profile.ts
@@ -1,6 +1,6 @@
 export class Profile{
     name: string = 'Julio Cesar';
     whatsApp: string = "'https://api.whatsapp.com/send?phone=5562996172261&text=ol%C3%A1,%20estou%20no%20seu%20site%20e%20cliquei%20para%20falar%20com%20voce'"
-    linkedIn: string = "www.linkedin.com/in/julio-cesar-dias-38a417264";
+    linkedIn: string = "https://www.linkedin.com/in/julio-cesar-c-dias/";
     email: string = "juliocesarcardosodias211@outlook.com";
 }
